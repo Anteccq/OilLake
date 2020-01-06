df924d75ed7f3abbd6204397f7af9b1958da3064
AntiqueR <39259186+AntiquePendulam@users.noreply.github.com>
2020-01-07 01:11:57 +0900

    Initial commit

---
+++ OilLakeHost/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Toolkit.Win32.UI.XamlHost;
+
+namespace OilLakeHost
+{
+    /// <summary>
+    /// 既定の Application クラスを補完するアプリケーション固有の動作を提供します。
+    /// </summary>
+    sealed partial class App : XamlApplication
+    {
+        public App()
+        {
+            Initialize();
+        }
+    }
+}
